02a4d392d55d404cd526dc4c6ae6127c8a71de49
douglas-evert <douglas2913@gmail.com>
2024-09-05 12:22:11 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "myVM" {
     }
 }
 
-resource "aws_instance" "myVM1" {
+resource "aws_instance" "myVM2" {
     ami = "ami-0490fddec0cbeb88b"
     instance_type = "t2.micro"
     vpc_security_group_ids = ["sg-aca77dc1"]
