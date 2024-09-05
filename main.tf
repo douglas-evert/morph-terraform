38a29ee93d14be1880bd7db35e28747c3d77c4b2
douglas-evert <douglas2913@gmail.com>
2024-09-05 12:01:34 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "myVM" {
+    ami = "ami-0490fddec0cbeb88b"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-aca77dc1"]
+	key_name = "Terraform Code"
+    tags = {
+        Name = var.hostname
+    }
+}
+
+provider "aws" {
+	region = "us-east-2"
+	access_key = var.access_key 
+	secret_key = var.secret_key
+}
+          
+variable "hostname" {
+          type = string
+          default = " "
+          description = "hostname and app name"
+}
+
+variable "access_key" {
+	type = string
+	default = " " 
+	description = "aws access key"
+}
+
+variable "secret_key" {
+	type = string
+	default = " " 
+	description = "aws secret key"
+}
