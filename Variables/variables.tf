0b64e13c537b39e2a211cb003e61e76d90604476
douglas-evert <douglas.evert@outlook.com>
2024-09-05 16:03:28 -0500

    add stuff

---
+++ Variables/variables.tf
@@ -0,0 +1,17 @@
+variable "hostname" {
+          type = string
+          default = " "
+          description = "hostname and app name"
+}
+
+variable "access_key" {
+	type = string
+	default = " " 
+	description = "aws access key"
+}
+
+variable "secret_key" {
+	type = string
+	default = " " 
+	description = "aws secret key"
+}
